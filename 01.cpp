9e18deed1a2ad67bed4b8ddb7ba3e9df1dd5eacd
Rahmad Nurhuda <rhmdnrhuda@gmail.com>
2018-11-03 01:08:30 +0700

    Modul Praktikum ASD
    Bahan untuk mengerjakan tugas Praktikum Algoritma Struktur Data.

---
+++ 01.cpp
@@ -0,0 +1,11 @@
+void PrintTera (List L) {
+/* I.S. List tidak mungkin kosong */
+/* F.S. isi list dicetak ke kanan: [e1,e2,...,en]  sampai ke */
+/* Contoh : jika ada tiga elemen bernilai A, B, C akan dicetak: 
+	[A,B,C]
+	[B,C,A]
+	[C,A,B]	  */
+/* Jika list kosong : menulis [] */
+/* Tidak ada tambahan karakter apa pun di awal, akhir, atau di tengah */
+	.......
+}
---
+++ 02.cpp
@@ -0,0 +1,6 @@
+/* Diperkenankan pula jika ingin menggunakan Fungsi */
+void HitungResource (List L, int n) {
+/* List tidak mungkin kosong */
+/* Jangan sampai pengambilan material melebihi n */
+
+}
